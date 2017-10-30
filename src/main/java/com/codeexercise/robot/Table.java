04740e054f621398b6e2a668592bf6d99c651131
Huy Ta <hlieuta@gmail.com>
2017-10-30 22:38:00 +1100

    Added missing java doc

--- src/main/java/com/codeexercise/robot/Table.java
+++ src/main/java/com/codeexercise/robot/Table.java
@@ -8,7 +8,7 @@ public interface Table {
 
     /**
      * Check if given point can be on the table
-     * @param location
+     * @param location The location <code> Location </code> to check.
      * @return true if point is on the table; false otherwise.
      */
     boolean isOnTable(Location location);
